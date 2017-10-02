ec462c4fa41bdbcb4211ae19b814c782b2d7723f
pjliddy <pjliddy@gmail.com>
2017-10-02 17:32:28 -0400

    Added notes on posts data object to comments
    * Added large comment before code in posts-index.js to explain the
      structure of a posts object referencing individual posts using id
      as a key (vs. using array of post objects)

--- src/components/posts-index.js
+++ src/components/posts-index.js
@@ -1,5 +1,20 @@
 import React, { Component } from 'react';
 
+/*
+  * Index API call gets back a list of post objects with properties.
+  * Storing posts in a `posts` object using ids as keys with data objects as
+    values:
+
+    {
+      4:  { title: "Hello", id: 4, content: "Hi", tags: "greetings" }.
+      12: { title: "Bye", id: 12, content: "Bye", tags: "greetings" }.
+    }
+
+    An individual post can then be referred to by id as:
+
+      state.posts[postId]
+*/
+
 class PostsIndex extends Component {
   render() {
     return(
