70040cab480d5f262e7d3c0484564a5f92fe21e5
pjliddy <pjliddy@gmail.com>
2017-10-02 16:05:30 -0400

    Added BrowserRouter with Route components
    * Replaced <App /> with <BrowserRouter>
    * Created test components for <Hello /> and <Goodbye />
    * Added <Route /> components to BrowserRouter with path and component
      params

--- src/index.js
+++ src/index.js
@@ -3,13 +3,39 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import App from './components/app';
 import reducers from './reducers';
 
+// app already set up to handle middleware
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+/*
+  Test components for routes
+*/
+
+class Hello extends React.Component {
+  render() { return <div>Hello!</div> };
+}
+
+class Goodbye extends React.Component {
+  render() { return <div>Goodbye!</div> };
+}
+
+/*
+  BrowserRouter can only have one child element, so multiple routes need to be wrapped in a <div>.
+  <Route> components have two parameters: path ('/hello') and component ({ Hello })
+*/
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <h1>Header on All Routes</h1>
+        <Route path="/hello" component={ Hello } />
+        <Route path="/goodbye" component={ Goodbye } />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
