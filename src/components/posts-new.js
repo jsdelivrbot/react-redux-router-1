37b70dfe730dca1596fd9330a19d709609fd8d75
pjliddy <pjliddy@gmail.com>
2017-10-03 15:01:48 -0400

    Added placeholder form to PostsNew component
    * Imported Field and reduxForm from redux-form
    * Added <form> with a placeholder title <Field /> to PostsNew layout

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
     return(
-      <div>PostsNew</div>
+      <form>
+        <Field
+          name="title"
+          component={}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+// reduxForm takes a configuration object as its argument:
+//  - `form` with a unique string
+
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
