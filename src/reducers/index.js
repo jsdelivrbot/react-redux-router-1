569e4bd5fddf863fddbb0331ff23f6732e42125f
pjliddy <pjliddy@gmail.com>
2017-10-03 14:50:55 -0400

    Added formReducer from redux-form
    * Installed npm package redux-form@6.3.3
    * imported reducer as formReducer into reducers/index.js
    * Added form: formReducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
+// import reducer from redux form and give a form-specific name
+import { reducer as formReducer } from 'redux-form';
+
 import PostsReducer from './reducer-posts';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
