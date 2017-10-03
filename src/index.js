5a6f035ef28e23e6837f295329f1be5747d26c1d
pjliddy <pjliddy@gmail.com>
2017-10-03 11:30:42 -0400

    Created placeholder PostsNew component
    * Created component in /components/posts-new.js with placeholder string
    * Added Route to <BrowserRouter /> in index.js with route to
      `/posts/new` using <PostsNew /> component
    
    NOTE: Route to `/posts/new` still showing PostsIndex component for `\`
          Bug will be resolved in future unit

--- src/index.js
+++ src/index.js
@@ -9,13 +9,14 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts-index';
+import PostsNew from './components/posts-new';
 
 // app already set up to handle middleware
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 /*
   BrowserRouter can only have one child element, so multiple routes need to be wrapped in a <div>.
-  
+
   <Route> components have two parameters: path ('/hello') and component ({ Hello })
 
   Using BrowserRouter, application no longer uses <App /> as its root component
@@ -26,6 +27,7 @@ ReactDOM.render(
     <BrowserRouter>
       <div className="container">
         <Route path="/" component={PostsIndex} />
+        <Route path="/posts/new" component={PostsNew} />
       </div>
     </BrowserRouter>
   </Provider>
