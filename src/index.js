3dc9e01ec27ee950444570133473e6c71cf51271
pjliddy <pjliddy@gmail.com>
2017-10-03 09:55:55 -0400

    Added reducer for posts
    * defined reducer in /reducers/reducer-posts.js
    * reducer returns posts object with id as key for each post
    * added posts to rootReducer in /reducers/index.js

--- src/index.js
+++ src/index.js
@@ -14,19 +14,8 @@ import PostsIndex from './components/posts-index';
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 /*
-  Test components for routes
-*/
-
-// class HelloComponent extends React.Component {
-//   render() { return <div>Hello!</div> };
-// }
-//
-// class GoodbyeComponent extends React.Component {
-//   render() { return <div>Goodbye!</div> };
-// }
-
-/*
   BrowserRouter can only have one child element, so multiple routes need to be wrapped in a <div>.
+  
   <Route> components have two parameters: path ('/hello') and component ({ Hello })
 
   Using BrowserRouter, application no longer uses <App /> as its root component
@@ -36,9 +25,6 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div className="container">
-        {/* <h1>React Router</h1> */}
-        {/* <Route path="/hello" component={ HelloComponent } /> */}
-        {/* <Route path="/goodbye" component={ GoodbyeComponent } /> */}
         <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
---
+++ src/reducers/reducer-posts.js
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+
+import FETCH_POSTS from '../actions';
+
+/*
+  Default state is an empty object
+
+  Need to take array of post records and make object with id props using
+  lodash _.mapKeys()
+*/
+
+export default function(state = {}, action) {
+  switch (action.type) {
+    case FETCH_POSTS:
+      return _.mapKeys(action.payload.data, 'id');
+    default:
+      return state;
+  }
+}
