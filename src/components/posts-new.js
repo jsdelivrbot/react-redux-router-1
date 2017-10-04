e51328dbc14f7e29c940bce3114c73d3e9f054a0
pjliddy <pjliddy@gmail.com>
2017-10-04 14:29:21 -0400

    Added validate function to Redux form
    * Created validate() function in PostsNew component
    * Added validation checks for field content in validate()
    * Added validate: validate to reduxForm export statement

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -8,6 +8,7 @@ import { Field, reduxForm } from 'redux-form';
 */
 
 class PostsNew extends Component {
+  // render label and input for <Field> component
   renderField(field) {
     return(
       <div className="form-group">
@@ -30,8 +31,8 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
         />
         <Field
@@ -44,9 +45,50 @@ class PostsNew extends Component {
   }
 }
 
-// reduxForm takes a configuration object as its argument:
-//  - `form` with a unique string
+/*
+  validation helper function
+  - values is the values the user has entered into the form
+
+  values -> {
+    title: "title",
+    categories: "category, category",
+    content: "content string"
+  }
+*/
+
+function validate(values) {
+  // return empty object if no errors
+  const errors = {};
+
+  // validate inputs from 'values'
+  if (values.title.length < 3) {
+    errors.title = "Enter a title for your post that is at least 3 characters.";
+  }
+
+  if (!values.title ) {
+    errors.title = "Enter a title for your post.";
+  }
+
+  if (!values.categories) {
+    errors.title = "Enter categories for your post.";
+  }
+
+  if (!values.content) {
+    errors.title = "Enter content for yoru new post.";
+  }
+
+  // if errors is an empty object, there are no issues with validation
+  // if errors has ANY properties, redux form assumes form is invalid
+  return errors;
+}
+
+/*
+  reduxForm takes a configuration object as its argument:
+   - 'validate' is the declared validate() function
+   - `form` with a unique string as ID
+*/
 
 export default reduxForm({
+  validate, // key and value are the same
   form: 'PostsNewForm'
 })(PostsNew);
