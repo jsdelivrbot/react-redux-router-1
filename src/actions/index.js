94f4011c993c58fc73f8fdc93e354c6b74b91141
pjliddy <pjliddy@gmail.com>
2017-10-05 15:42:25 -0400

    Created delete post action
    * created deletePost action handler to return action with type:
      DELETE_POST and payload of id
    * created onDeleteClick event handler that calls deletePost action
      with navigation callback to '/' after post is deleted

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_post';
+export const DELETE_POST = 'delete_post';
 
 export const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 export const API_KEY = '?key=s9k93K32K09xl1jfh8';
@@ -41,11 +42,32 @@ export function createPost(values, callback) {
   }
 }
 
+/*
+  make get AJAX request to /posts/:id
+*/
+
 export function fetchPost(id) {
   const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
 
   return {
     type: FETCH_POST,
     payload: request
+  }
+}
+
+/*
+  make delete AJAX request to /posts/:id
+  - return id of post being deleted to be passed to reducer to delete
+    post from state
+  - include callback for navigation after successful delete
+*/
+
+export function deletePost(id, callback) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+    .then(() => callback());
+
+  return {
+    type: DELETE_POST,
+    payload: id
   }
 }
--- src/components/posts-show.js
+++ src/components/posts-show.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions';
+import { deletePost } from '../actions';
 import { Link } from 'react-router-dom';
 
 /*
@@ -19,6 +20,16 @@ class PostsShow extends Component {
     }
   }
 
+  // get id from this.props.match.params
+  // pass navigation callback (.push('/')) for after delete is successful
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+    
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
+  }
+
   render() {
     const { post } = this.props;
 
@@ -29,7 +40,11 @@ class PostsShow extends Component {
     return (
       <div>
         <Link to="/">Back to Index</Link>
-
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}>
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -49,4 +64,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id]};
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
