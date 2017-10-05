804b1623cd8875ad1c086424992cfb48db83e040
pjliddy <pjliddy@gmail.com>
2017-10-05 14:16:21 -0400

    Added componentDidMount() to PostsShow
    * use this.props.match.params to get post id from URL
    * pass id to this.props.fetchPost(id)
    * pass ownProps to mapStateToProps() to return posts[id] as post
    * add mapStateToProps as first argument to connect()(PostsShow)

--- src/components/posts-show.js
+++ src/components/posts-show.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    // provided by react router as param `id`
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
+    // const post = this.props.post;
+    // console.log(post);
+
     return (
       <div>
         Posts Show
+        {/* <p>{post.title}</p>
+          <p>{post.categories}</p>
+        <p>{post.content}</p> */}
       </div>
     );
   }
 }
 
-export default PostsShow;
+// use second argument, ownProps, the props for this component
+// this.props === ownProps
+
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id]};
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
