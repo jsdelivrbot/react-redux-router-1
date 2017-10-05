a9d414a7b9c643617828b8fdad850d6e5dba11cd
pjliddy <pjliddy@gmail.com>
2017-10-05 14:23:20 -0400

    Added conditional logic for PostsShow loading
    * added if (!post) to return 'Loading...' message if post data has not
      been returned before component is rendered

--- src/components/posts-show.js
+++ src/components/posts-show.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions';
 
+/*
+
+*/
+
 class PostsShow extends Component {
   componentDidMount() {
     // provided by react router as param `id`
@@ -10,22 +14,28 @@ class PostsShow extends Component {
   }
 
   render() {
-    // const post = this.props.post;
-    // console.log(post);
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
 
     return (
       <div>
-        Posts Show
-        {/* <p>{post.title}</p>
-          <p>{post.categories}</p>
-        <p>{post.content}</p> */}
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
 }
 
-// use second argument, ownProps, the props for this component
-// this.props === ownProps
+/*
+  use second argument, ownProps, the props for this component
+  this.props === ownProps
+
+  post is undefined when application first loads
+*/
 
 function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id]};
